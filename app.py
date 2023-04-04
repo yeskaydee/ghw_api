fe8925c66ef7aedc53f96f87198884808c9d15c9
Santosh Kumar Doodala <84093797+yeskaydee@users.noreply.github.com>
2023-04-04 12:31:02 +0530

    final push is made

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_ghw():
-    return "<p>Hello new user!</p>"
+    return "<p>Hello new users --final push!</p>"
 if __name__ == "__main__":
     app.run(debug=True)
