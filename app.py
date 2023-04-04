a5399f3703070f6a268ab334fa0342026fc888a9
Santosh Kumar Doodala <84093797+yeskaydee@users.noreply.github.com>
2023-04-04 12:26:53 +0530

    reverse

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_ghw():
-    return "<p>Hello, new</p>"
+    return "<p>Hello, API Week hackers!</p>"
 if __name__ == "__main__":
     app.run(debug=True)
